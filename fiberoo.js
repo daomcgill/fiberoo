7e2c36b82e4ec8403012afdfd62fd0b07211b4a0
daomcgill <77309217+daomcgill@users.noreply.github.com>
2023-09-18 10:05:51 -1000

    Update fiberoo.js

--- fiberoo.js
+++ fiberoo.js
@@ -1,26 +1,31 @@
-function fib(){
+function fib() {
   let arr = [];
-  for (let i = 0; i < 50; i++){
-    if (i < 2){
-    arr.push(i);
+  for (let i = 0; i < 50; i++) {
+    if (i < 2) {
+      arr.push(i);
     } else {
-      arr.push(arr[i-1] + arr[i-2]);
+      arr.push(arr[i - 1] + arr[i - 2]);
     }
   }
   return arr;
 }
+
 console.log(fib());
-function numToStrings(arr){
-  let strArr = _.map(arr, function(num){
+
+function numToStrings(arr) {
+  let strArr = _.map(arr, function (num) {
     return num.toString();
   });
   return strArr;
 }
+
 console.log(numToStrings(fib()));
-function numEvenNums(arr){
-  let evenNums = _.filter(arr, function(num){
+
+function numEvenNums(arr) {
+  let evenNums = _.filter(arr, function (num) {
     return num % 2 === 0;
   });
   return evenNums.length;
 }
+
 console.log(numEvenNums(fib()));
